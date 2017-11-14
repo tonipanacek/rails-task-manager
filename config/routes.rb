1b180370d8ad96c1ea230fd5c0197af2f6f62a28
Toni Panacek <toni.panacek@gmail.com>
2017-11-14 14:26:13 +0100

    refactored some code

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
   # create --> new
-  get 'tasks/new', to:'tasks#new', as: :new_task
+  # get 'tasks/new', to:'tasks#new', as: :new_task
 
-  # read --> index
-  get 'tasks', to: 'tasks#index', as: :tasks
+  # # read --> index
+  # get 'tasks', to: 'tasks#index', as: :tasks
 
-  # read --> show
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # read --> show
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # create --> create
-  post 'tasks', to: 'tasks#create'
+  # # create --> create
+  # post 'tasks', to: 'tasks#create'
 
-  # update --> edit
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # # update --> edit
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 
-  # update --> update
-  patch 'tasks/:id', to: 'tasks#update'
+  # # update --> update
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # delete --> destroy
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # delete --> destroy
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
+
+  root to: 'tasks#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
