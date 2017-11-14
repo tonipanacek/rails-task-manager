b77513c50181fca2b7fe548faa5e2802191e28df
Toni Panacek <toni.panacek@gmail.com>
2017-11-14 14:07:50 +0100

    to do controller views and routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  # create --> new
+  get 'tasks/new', to:'tasks#new', as: :new_task
+
+  # read --> index
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  # read --> show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # create --> create
+  post 'tasks', to: 'tasks#create'
+
+  # update --> edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # update --> update
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete --> destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: 'laundry', description: 'do laundry asap')
+
+Task.create(name: 'dishes', description: 'clean all dishes in sink')
